2b0f3854573b0fcb663c11eb64bcf157b377b7bf
Andrew Galushka <andrewgalushka@yahoo.com>
2019-04-04 15:36:14 +0300

    rf

--- Giphity/Main/AppDelegate.swift
+++ Giphity/Main/AppDelegate.swift
@@ -15,8 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         self.window = UIWindow(frame: UIScreen.main.bounds)
-        self.window?.backgroundColor = .cyan
-        self.window?.rootViewController = UIViewController()
+        self.window?.rootViewController = RandomGifViewController.loadFromStoryboard()
         self.window?.makeKeyAndVisible()
         
         return true
--- Giphity/Screens/RandomGif/RandomGifViewController.swift
+++ Giphity/Screens/RandomGif/RandomGifViewController.swift
@@ -9,9 +9,28 @@
 import UIKit
 
 class RandomGifViewController: UIViewController {
-
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nextRandomGifButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupUI()
+    }
+    
+    func setupUI() {
+        nextRandomGifButton.setTitle("Next Random Gif", for: .normal)
+        nextRandomGifButton.backgroundColor = .orange
+        nextRandomGifButton.tintColor = .white
+        nextRandomGifButton.layer.cornerRadius = 5.0
+        
+        nextRandomGifButton.setNeedsLayout()
+        nextRandomGifButton.layoutIfNeeded()
+        nextRandomGifButton.widthAnchor.constraint(equalToConstant: nextRandomGifButton.bounds.width + 20).isActive = true
+    }
+    
+    @IBAction func nextRandomGifButtonTouchUpInsideActionHandler(_ sender: Any) {
+        
     }
 }
 
