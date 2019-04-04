4156adef43819441f6fda25b863ede4eb4b97504
Andrew Galushka <andrewgalushka@yahoo.com>
2019-04-04 13:52:30 +0300

    Remove entry via main storyboard, show stub UIViewController from AppDelegate

--- Giphity/Main/AppDelegate.swift
+++ Giphity/Main/AppDelegate.swift
@@ -13,8 +13,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.backgroundColor = .cyan
+        self.window?.rootViewController = UIViewController()
+        self.window?.makeKeyAndVisible()
+        
         return true
     }
 
