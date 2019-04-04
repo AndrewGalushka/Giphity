4c91f7dde3d7174747c347d0a2257792490317e3
Andrew Galushka <andrewgalushka@yahoo.com>
2019-04-04 13:36:29 +0300

    initial commit

---
+++ Giphity/Main/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Giphity
+//
+//  Created by Galushka on 4/4/19.
+//  Copyright © 2019 Galushka. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
+
---
+++ Giphity/Screens/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Giphity
+//
+//  Created by Galushka on 4/4/19.
+//  Copyright © 2019 Galushka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
