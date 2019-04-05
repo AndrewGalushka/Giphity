b8df7415bf27535f524fb6dab607170e040b3fb8
Galushka Andrew <andrewgalushka@yahoo.com>
2019-04-05 13:55:05 +0300

    Added SearchGifsViewController

---
+++ Giphity/Screens/SearchGifs/SearchGifsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SearchGifsViewController.swift
+//  Giphity
+//
+//  Created by Galushka on 4/5/19.
+//  Copyright © 2019 Galushka. All rights reserved.
+//
+
+import UIKit
+
+class SearchGifsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
