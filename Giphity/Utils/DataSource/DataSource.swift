46860e61577031bffafa91f94f35418eec97fb52
Andrew Galushka <andrewgalushka@yahoo.com>
2019-04-08 18:45:23 +0300

    Add DataSource and Section

---
+++ Giphity/Utils/DataSource/DataSource.swift
@@ -0,0 +1,26 @@
+//
+//  DataSource.swift
+//  Giphity
+//
+//  Created by Galushka on 4/8/19.
+//  Copyright © 2019 Galushka. All rights reserved.
+//
+
+import Foundation
+
+struct DataSource<Item> {
+    var sections: [Section<Item>]
+    
+    func numberOfSections() -> Int {
+        return sections.count
+    }
+    
+    func numberOfItems(in section: Int) -> Int {
+        guard section < sections.count else { return 0 }
+        return sections[section].items.count
+    }
+    
+    func item(at indexPath: IndexPath) -> Item {
+        return sections[indexPath.section].items[indexPath.row]
+    }
+}
---
+++ Giphity/Utils/DataSource/Section.swift
@@ -0,0 +1,13 @@
+//
+//  Section.swift
+//  Giphity
+//
+//  Created by Galushka on 4/8/19.
+//  Copyright © 2019 Galushka. All rights reserved.
+//
+
+import Foundation
+
+struct Section<Item> {
+    var items: [Item]
+}
