d37cb6b03a6da85fbef6dcba615806986d8b2f99
Andrew Galushka <andrewgalushka@yahoo.com>
2019-04-09 11:00:19 +0300

    add CollectionViewConfigurator

--- Giphity/Utils/CollectionViewConfigurator/CollectionViewConfigurator.swift
+++ Giphity/Utils/CollectionViewConfigurator/CollectionViewConfigurator.swift
@@ -26,3 +26,29 @@ extension CollectionViewConfiguratorType {
         return self.configure(cell: cell, item: item, collectionView: collectionView, indexPath: indexPath)
     }
 }
+
+struct CollectionViewConfigurator<Item, Cell: UICollectionViewCell>: CollectionViewConfiguratorType {
+    typealias CellConfigurator = (Cell, Item, UICollectionView, IndexPath) -> Cell
+    
+    let reuseIdentifier: String = "\(Cell.self)"
+    let configurator: CellConfigurator
+    
+    func reuseIdentifier(for item: Item, indexPath: IndexPath) -> String {
+        return reuseIdentifier
+    }
+    
+    func configure(cell: Cell, item: Item, collectionView: UICollectionView, indexPath: IndexPath) -> Cell {
+        return configurator(cell, item, collectionView, indexPath)
+    }
+    
+    func registerCells(in collectionView: UICollectionView) {
+        
+        if let path = Bundle.main.path(forResource: "\(Cell.self)", ofType: "nib"),
+            FileManager.default.fileExists(atPath: path) {
+            let nib = UINib(nibName: "\(Cell.self)", bundle: .main)
+            collectionView.register(nib, forCellWithReuseIdentifier: reuseIdentifier)
+        } else {
+            collectionView.register(Cell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        }
+    }
+}
