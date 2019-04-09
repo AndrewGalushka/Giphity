018d005b1514629824b139c73736fda013da0e81
Andrew Galushka <andrewgalushka@yahoo.com>
2019-04-09 18:30:09 +0300

    Add GifCollectionViewCellViewModel

---
+++ Giphity/Screens/SearchGifs/GifCollectionViewCell/GifCollectionViewCellViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  GifCollectionViewCellViewModel.swift
+//  Giphity
+//
+//  Created by Galushka on 4/9/19.
+//  Copyright © 2019 Galushka. All rights reserved.
+//
+
+import Foundation
+
+extension GifCollectionViewCell {
+    
+    struct ViewModel {
+        let identifier: String
+        let gifURL: String
+    }
+}
