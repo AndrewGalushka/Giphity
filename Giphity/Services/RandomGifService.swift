9b62ab80c579e7ecb16f562b36995c1c9816d75b
Andrew Galushka <andrewgalushka@yahoo.com>
2019-04-18 12:43:40 +0300

    Add implementation to RandomGifServiceType

--- Giphity/Services/RandomGifService.swift
+++ Giphity/Services/RandomGifService.swift
@@ -10,5 +10,26 @@ import Foundation
 import PromiseKit
 
 class RandomGifService {
+    private let gifFetcher: GifFetcherType
+    private let requestManager: RandomGifRequestPerformable
     
+    init(gifFetcher: GifFetcherType, requestManager: RandomGifRequestPerformable) {
+        self.gifFetcher = gifFetcher
+        self.requestManager = requestManager
+    }
+    
+    func randomGif() -> Promise<UIImage> {
+        
+        return PromiseKit.firstly {
+            self.requestManager.randomGif()
+        }.then { (response) -> Promise<UIImage> in
+            let gifObject = try self.gifObject(from: response)
+            return self.gifFetcher.fetch(gifObject)
+        }
+    }
+    
+    func gifObject(from response: GiphyResponse<GifObject>) throws -> GifObject {
+        guard let gifObject = response.data else { throw RandomGifServiceError.emptyData }
+        return gifObject
+    }
 }
