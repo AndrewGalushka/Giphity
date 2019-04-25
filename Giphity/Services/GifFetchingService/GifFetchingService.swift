10cdd42ec2362c56fbcd13b938f023e50d2f9c7e
Andrew Galushka <andrewgalushka@yahoo.com>
2019-04-25 17:42:14 +0300

    Add basic imp to GifFetchingService

--- Giphity/Services/GifFetchingService/GifFetchingService.swift
+++ Giphity/Services/GifFetchingService/GifFetchingService.swift
@@ -10,27 +10,85 @@ import Foundation
 import PromiseKit
 
 class GifFetchingService: GifFetchingServiceType {
-    private let operationQueue: OperationQueue = {
-        let queue = OperationQueue()
-        queue.name = "com.gif.fetching.service.queue"
-        
-        return queue
-    }()
+    
+    let gifFetcher: GifFetcherType
+    
+    init(fetcher: GifFetcherType) {
+        self.gifFetcher = fetcher
+    }
     
     func fetchGif(using url: URL) -> Promise<UIImage> {
-        let extFetcingTask = self.operationQueue.operations.first { (operation) -> Bool in
-            return url.path == operation.name
-        }
+        let (promise, resolver) = Promise<UIImage>.pending()
         
-        if let extFetcingTask = extFetcingTask {
-            return Promise.init(resolver: { (resolver) in
-                DispatchQueue.global(qos: .userInteractive).async {
-                    extFetcingTask.waitUntilFinished()
-                    
+        self.pendingTask(for: url).done { [weak self] (fetchingTask) in
+            guard let strongSelf = self else { return }
+            
+            if let fetchingTask = fetchingTask {
+                fetchingTask.done {
+                    resolver.fulfill($0)
+                }.catch {
+                    resolver.reject($0)
                 }
-            })
+                
+                return
+            } else {
+                let fetchingTask = strongSelf.gifFetcher.fetch(url)
+                
+                strongSelf.addPendingTask(url: url, task: fetchingTask)
+                strongSelf.removePendingTask(at: url, afterCompetionOf: fetchingTask)
+                
+                fetchingTask.done {
+                    resolver.fulfill($0)
+                }.catch {
+                    resolver.reject($0)
+                }
+            }
         }
         
-        fatalError()
+        return promise
+    }
+    
+    // MARK: - Pending Fetching Tasks
+    
+    private let pendingTasksReadWriteQueue: OperationQueue = {
+        let queue = OperationQueue()
+        queue.maxConcurrentOperationCount = 1
+        return queue
+    }()
+    private var pendingTasks = [URL: Promise<UIImage>]()
+    
+    @discardableResult private func addPendingTask(url: URL, task: Promise<UIImage>) -> Guarantee<()> {
+        return Guarantee<Void>.init(resolver: { (resolver) in
+            pendingTasksReadWriteQueue.addOperation {
+                self.pendingTasks[url] = task
+                resolver(())
+            }
+        })
+    }
+    
+    private func removePendingTask(at url: URL, afterCompetionOf task: Promise<UIImage>) {
+        
+        _ = task.ensure { [weak self] in
+            guard let strongSelf = self else { return }
+            strongSelf.removePendingTask(for: url)
+        }
+    }
+    
+    @discardableResult private func removePendingTask(for url: URL) -> Guarantee<()> {
+        return Guarantee<Void>.init(resolver: { (resolver) in
+            pendingTasksReadWriteQueue.addOperation {
+                self.pendingTasks[url] = nil
+                resolver(())
+            }
+        })
+    }
+    
+    private func pendingTask(for url: URL) -> Guarantee<Promise<UIImage>?> {
+        return Guarantee<Promise<UIImage>?>.init(resolver: { (resolver) in
+            pendingTasksReadWriteQueue.addOperation {
+                let pendingTask = self.pendingTasks[url]
+                resolver(pendingTask)
+            }
+        })
     }
 }
