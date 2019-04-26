5c3abe41551eaab5819510787a4b4b22ef3a5c46
Andrew Galushka <andrewgalushka@yahoo.com>
2019-04-26 11:01:15 +0300

    rf

---
+++ Giphity/Screens/SearchGifs/CollectionView/GifCollectionViewCell/GifCollectionViewCell.swift
@@ -0,0 +1,73 @@
+//
+//  GifCollectionViewCell.swift
+//  Giphity
+//
+//  Created by Galushka on 4/5/19.
+//  Copyright © 2019 Galushka. All rights reserved.
+//
+
+import UIKit
+
+class GifCollectionViewCell: UICollectionViewCell {
+
+    // MARK: - Properties(Private)
+    
+    @IBOutlet private weak var imageView: UIImageView!
+    
+    private var viewModel: ViewModel?
+    private let gifFetcher: GifFetcher = GifFetcher()
+    
+    // MARK: - Lifecycle
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        self.viewModel = nil
+        self.imageView.image = nil
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        self.contentView.backgroundColor = UIColor.rgba(244, 255, 250)
+    }
+    
+    func configure(_ viewModel: ViewModel) {
+        self.viewModel = viewModel
+    }
+    
+    func displayGif() {
+        guard let viewModel = self.viewModel else { return }
+        
+        let fetchingGifID = viewModel.identifier
+        
+        var fetchingTimeLogger = TimeSpentLogger()
+        fetchingTimeLogger.start()
+        
+        self.gifFetcher.fetch(viewModel.gifURL) { [weak self] (response) in
+            
+            fetchingTimeLogger.end(textBeforeTimeLog: "Gif fetching time is")
+            
+            guard
+                let currentViewModel = self?.viewModel,
+                currentViewModel.identifier == fetchingGifID
+            else { return }
+            
+            var imageSettingTimeLogger = TimeSpentLogger()
+            imageSettingTimeLogger.start()
+            
+            DispatchQueue.main.async {
+                switch response {
+                case .success(let gif):
+                    self?.imageView.image = gif
+                case .failure(_):
+                    self?.imageView.image = nil
+                }
+                
+                imageSettingTimeLogger.end(textBeforeTimeLog: "Time spent to set image is")
+            }
+        }
+    }
+}
+
+extension GifCollectionViewCell: InterfaceBuilderIdentifiable {}
