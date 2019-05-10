7d596ea149faa2397c7901797fe3b36775bc98e9
Andrew Galushka <andrewgalushka@yahoo.com>
2019-05-10 17:56:24 +0300

    Add description to limit property

--- Giphity/Services/SearchGIFsPaginationService/SearchGIFsPaginationService.swift
+++ Giphity/Services/SearchGIFsPaginationService/SearchGIFsPaginationService.swift
@@ -13,6 +13,7 @@ class SearchGIFsPaginationService: SearchGIFsPaginationServiceType {
     // MARK: - Properties(Public)
     
     weak var delegate: SearchGIFsPaginationServiceDelegate?
+    /// Max value can't be highter the 25
     var limit: UInt = UInt(15)
     
     // MARK: - Properties(Private)
