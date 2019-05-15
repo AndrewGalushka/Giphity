61d158428e2fd2956271656fd6de4ccf68c009e7
Andrew Galushka <andrewgalushka@yahoo.com>
2019-05-15 10:35:46 +0300

    Add TrandingGIFsPresenter

---
+++ Giphity/Screens/TrandingGIFs/TrandingGIFsPresenter/TrandingGIFsPresenter.swift
@@ -0,0 +1,20 @@
+//
+//  TrandingGIFsPresenter.swift
+//  Giphity
+//
+//  Created by Galushka on 5/15/19.
+//  Copyright © 2019 Galushka. All rights reserved.
+//
+
+import Foundation
+
+class TrandingGIFsPresenter: TrandingGIFsViewPresenter {
+    
+    // MARK: - TrandingGIFsViewPresenter Imp
+    
+    func viewLoaded() {
+    }
+    
+    func trandingGIFs() {
+    }
+}
---
+++ Giphity/Screens/TrandingGIFs/TrandingGIFsPresenter/TrandingGIFsViewPresenter.swift
@@ -0,0 +1,14 @@
+//
+//  TrandingGIFsViewPresenter.swift
+//  Giphity
+//
+//  Created by Galushka on 5/15/19.
+//  Copyright © 2019 Galushka. All rights reserved.
+//
+
+import Foundation
+
+protocol TrandingGIFsViewPresenter {
+    func viewLoaded()
+    func trandingGIFs()
+}
