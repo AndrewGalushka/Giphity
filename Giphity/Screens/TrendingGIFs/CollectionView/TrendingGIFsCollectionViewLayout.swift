56847d9ea8743b34ac9bb2ced858813cd8d672aa
Andrew Galushka <andrewgalushka@yahoo.com>
2019-05-17 15:05:43 +0300

    Add TrendingGIFsCollectionViewLayout

---
+++ Giphity/Screens/TrendingGIFs/CollectionView/TrendingGIFsCollectionViewLayout.swift
@@ -0,0 +1,38 @@
+//
+//  TrendingGIFsCollectionViewLayout.swift
+//  Giphity
+//
+//  Created by Galushka on 5/17/19.
+//  Copyright © 2019 Galushka. All rights reserved.
+//
+
+import UIKit
+
+class TrendingGIFsCollectionViewLayout: UICollectionViewFlowLayout {
+    override func prepare() {
+        super.prepare()
+        
+        guard let collectionView = self.collectionView else { return }
+        
+        self.recalculateIndents(for: collectionView)
+        self.recalculateItemSize(for: collectionView)
+    }
+    
+    private func recalculateIndents(for collectionView: UICollectionView) {
+        self.minimumInteritemSpacing = 5.0
+        self.minimumLineSpacing = 5.0
+        
+        self.sectionInset = UIEdgeInsets(top: self.minimumInteritemSpacing,
+                                         left: self.minimumInteritemSpacing,
+                                         bottom: self.minimumInteritemSpacing,
+                                         right: self.minimumInteritemSpacing)
+    }
+    
+    private func recalculateItemSize(for collectionView: UICollectionView) {
+        let numberOfItemsInRow = 2
+        let sideSize = (collectionView.bounds.inset(by: collectionView.layoutMargins).width / CGFloat(numberOfItemsInRow)).rounded(.down)
+        
+        self.itemSize = CGSize(width: sideSize,
+                               height: sideSize)
+    }
+}
