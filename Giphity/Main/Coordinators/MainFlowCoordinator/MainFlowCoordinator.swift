81ac417e781b8e88db01ef3177143f77bdcfc398
Andrew Galushka <andrewgalushka@yahoo.com>
2019-05-28 17:51:43 +0300

    Add Icons to tab bar items, wrap UITabBarController with UINavigationController

--- Giphity/Main/Coordinators/MainFlowCoordinator/MainFlowCoordinator.swift
+++ Giphity/Main/Coordinators/MainFlowCoordinator/MainFlowCoordinator.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class MainFlowCoordinator: FlowCoordinatorType {
     private let window: UIWindow
+    private let navigationController: UINavigationController
     private let tabbarController: UITabBarController
     private let assembler: ApplicationAssemblerType
     private let modulesAssembler: MainFlowCoordinatorModulesAssemblerType
@@ -22,29 +23,20 @@ class MainFlowCoordinator: FlowCoordinatorType {
         self.modulesAssembler = MainFlowCoordinatorModulesAssembler(assembler: assembler)
         
         self.tabbarController = UITabBarController()
+        self.navigationController = UINavigationController()
     }
     
     func start() {
-        let trendingGIFsModule = self.modulesAssembler.assembleTrendingGIFsModule()
-        let randomGIFsModule = self.modulesAssembler.assembleRandomGifModule()
-        let searchGIFsModule = self.modulesAssembler.assembleSearchGIFsModule()
-        
-        let initialTabbarModules = [trendingGIFsModule, randomGIFsModule, searchGIFsModule]
-        
+        let initialTabbarModules = makeInitialTabbarModules()
         self.addModules(initialTabbarModules)
         
-        trendingGIFsModule.asViewController.tabBarItem.title = "TRENDING"
-        randomGIFsModule.asViewController.tabBarItem.title = "RANDOM"
-        searchGIFsModule.asViewController.tabBarItem.title = "SEARCH"
+        tabbarController.setViewControllers(initialTabbarModules.map { $0.asViewController }, animated: true)
+        tabbarController.selectedIndex = 2
         
-        let titleOffset = UIOffset(horizontal: 0.0, vertical: -5.0)
-        initialTabbarModules.forEach { $0.asViewController.tabBarItem.titlePositionAdjustment = titleOffset }
+        navigationController.setViewControllers([tabbarController], animated: false)
+        navigationController.setNavigationBarHidden(true, animated: false)
         
-        tabbarController.setViewControllers(initialTabbarModules.map { $0.asViewController },
-                                            animated: false)
-        tabbarController.selectedIndex = 1
-        
-        window.rootViewController = tabbarController
+        window.rootViewController = navigationController
     }
 
     func addModule(_ module: ViewControllerModule) {
@@ -57,5 +49,19 @@ class MainFlowCoordinator: FlowCoordinatorType {
     
     func removeModule(_ module: ViewControllerModule) {
         modules.removeAll { $0 === module }
+    }
+    
+    private func makeInitialTabbarModules() -> [ViewControllerModule] {
+        let trendingGIFsModule = self.modulesAssembler.assembleTrendingGIFsModule()
+        let randomGIFsModule = self.modulesAssembler.assembleRandomGifModule()
+        let searchGIFsModule = self.modulesAssembler.assembleSearchGIFsModule()
+        
+        let initialTabbarModules = [trendingGIFsModule, randomGIFsModule, searchGIFsModule]
+        
+        trendingGIFsModule.asViewController.tabBarItem = UITabBarItem(title: "TRENDING", image: UIImage(named: "tab_bar_icon_selected"), selectedImage: UIImage(named: "tab_bar_icon_selected"))
+        randomGIFsModule.asViewController.tabBarItem = UITabBarItem(title: "RANDOM", image: UIImage(named: "tab_bar_icon_selected"), selectedImage: UIImage(named: "tab_bar_icon_selected"))
+        searchGIFsModule.asViewController.tabBarItem = UITabBarItem(title: "SEARCH", image: UIImage(named: "tab_bar_icon_selected"), selectedImage: UIImage(named: "tab_bar_icon_selected"))
+        
+        return initialTabbarModules
     }
 }
